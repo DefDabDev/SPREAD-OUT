a7cb1b411bc7a05973ccf0dca4e172535de77332
Kim Deokwon <64bit_decryption@naver.com>
2017-07-11 21:45:08 +0900

    Character State Base

---
+++ Assets/InGame/Script/Character/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    [SerializeField]
+    private List<State> _states;
+
+    [SerializeField]
+    private string _currentStateName;
+
+    private State _currentState = null;
+
+	void Awake ()
+    {
+        for (int i = 0; i < _states.Count; ++i)
+            _states[i].Init();
+
+        ChangeState(StateNames.runState);
+	}
+	
+	void Update ()
+    {
+        if (_currentState != null)
+            _currentState.Doing();
+	}
+
+    public State GetState(string name)
+    {
+        for (int i = 0; i < _states.Count; ++i)
+            if (_states[i].stateName.Equals(name))
+                return _states[i];
+        Debug.LogError(string.Format("Can't Find State! Name ({0})", name));
+        return null;
+    }
+
+    public void ChangeState(string name)
+    {
+        ChangeState(GetState(name));
+    }
+
+    public void ChangeState(State state)
+    {
+        if (_currentState != null)
+            _currentState.ToChange();
+        _currentState = state;
+        _currentState.OnChange();
+    }
+}
---
+++ Assets/InGame/Script/Character/SpriteAnimator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class SpriteAnimation
+{
+    public string name;
+    public float fps;
+    public bool isLoop;
+    public Sprite[] clips;
+}
+
+public class SpriteAnimator : MonoBehaviour {
+
+    [SerializeField]
+    private List<SpriteAnimation> _animationState = null;
+
+    private SpriteAnimation _currentAnimation = null;
+    private Image _image = null;
+    private float _timer = 0f;
+    private uint _index = 0;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void ChangeState(string name)
+    {
+        _index = 0;
+        _timer = 0f;
+
+        for (int i = 0; i < _animationState.Count; ++i)
+        {
+            if (_animationState[i].name.Equals(name))
+            {
+                _currentAnimation = _animationState[i];
+                break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_currentAnimation == null)
+            return;
+
+        _timer += Time.deltaTime;
+        if (_timer > _currentAnimation.fps)
+        {
+            _timer = 0f;
+            ++_index;
+            if (_currentAnimation.clips.Length <= _index)
+            {
+                if (_currentAnimation.isLoop)
+                    _index = 0; 
+                else
+                    --_index;
+            }
+            _image.sprite = _currentAnimation.clips[_index];
+        }
+    }
+}
