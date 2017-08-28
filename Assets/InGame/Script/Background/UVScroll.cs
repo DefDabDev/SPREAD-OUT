74fb5ff9880946c38639530fa350118934b9e54b
KimDeokwon <64bit_decryption@naver.com>
2017-08-29 02:43:58 +0900

    Add UVscroll
    Signed-off-by: KimDeokwon <64bit_decryption@naver.com>

---
+++ Assets/InGame/Script/Background/UVScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UVScroll : MonoBehaviour {
+
+	[SerializeField]
+	private float _speed = 1f;
+
+	private float _x = 0f;
+
+	private RawImage _rawImage = null;
+
+	private void Awake()
+	{
+		_rawImage = GetComponent<RawImage>();
+	}
+
+	private void Update()
+	{
+		_x += Time.smoothDeltaTime * _speed;
+		_rawImage.uvRect = new Rect(_x, 0f, 1f, 1f);
+	}
+}
