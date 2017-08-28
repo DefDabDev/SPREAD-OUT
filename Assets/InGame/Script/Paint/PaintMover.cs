31c65af7d27b5e8850f7a9d2ad8acd05a2c9f348
KimDeokwon <64bit_decryption@naver.com>
2017-08-29 02:09:26 +0900

    Update scene, something else
    Signed-off-by: KimDeokwon <64bit_decryption@naver.com>

---
+++ Assets/InGame/Script/Paint/PaintMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintMover : MonoBehaviour {
+
+	[SerializeField]
+	private float _speed = 1f;
+	
+	private Rigidbody2D _rigidBody2D = null;
+
+	private void Awake()
+	{
+		_rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() 
+	{
+		_rigidBody2D.velocity = transform.up * _speed;
+	}
+
+	public void SetPaint(float angle)
+	{
+		transform.localRotation = Quaternion.Euler(0f, 0f, angle);
+	}
+}
