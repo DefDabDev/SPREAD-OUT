943d17b78d56884c28cb87af23deccb12fd96e51
KimDeokwon <64bit_decryption@naver.com>
2017-09-07 00:04:46 +0900

    Update Util
    Signed-off-by: KimDeokwon <64bit_decryption@naver.com>

---
+++ Assets/Library/ALUtil/ALLog/ALLog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL.ALLog
+{
+	public static class ALLog 
+	{
+		public static void Log(object message)
+		{
+		#if UNITY_EDITOR
+			if (Application.isEditor)
+				Debug.Log(message);
+		#endif
+		}
+
+		public static void WarningLog(object message)
+		{
+		#if UNITY_EDITOR
+			if (Application.isEditor)
+				Debug.LogWarning(message);
+		#endif
+		}
+
+		public static void ErrorLog(object message)
+		{
+		#if UNITY_EDITOR
+			if (Application.isEditor)
+				Debug.LogError(message);
+		#endif
+		}
+	}
+}
