62ca0e062a1fd2cf9492571fe48a58037701d2bf
kyechan99 <kyechan99@naver.com>
2017-09-17 22:04:15 +0900

    map - create random

--- Assets/InGame/Script/MapLoad.cs
+++ Assets/InGame/Script/MapLoad.cs
@@ -3,33 +3,61 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+//////////////////
+// 일단 텍스트 불러오는걸로 하자 !
+///
+// 그래 ! 그게 좋겠따 !
+//////////////////
 public class MapLoad : MonoBehaviour
 {
     Vector2 basicPos;
     [SerializeField]
     GameObject tileObj;
 
+    int posX = 0, posY = 0;
+
     void Start()
     {
         basicPos = new Vector2(0, -710);
-        Debug.Log(readStringFromFile("MapData/map0"));
+        createMap(); createMap(); createMap(); createMap();
     }
 
-    string readStringFromFile(string fileName)
+    /// <summary>
+    /// createMap
+    /// </summary>
+    /// <desc>
+    /// 맵 불러올일 있을때 이거 실행시키면됨
+    /// </desc>
+    public void createMap()
+    {
+        Debug.Log(loadMap("MapData/map" + Random.Range(0, 4)));
+    }
+
+    /// <summary>
+    /// Load Map
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    string loadMap(string fileName)
     {
         TextAsset textAsset = Resources.Load(fileName) as TextAsset;
         string str = textAsset.text;
 
-        string[] spString = str.Split('\n');
+        basicPos = new Vector2(posX * 300, posY * 300 - 710);
 
+        string[] spString = str.Split('\n');
+        string[] code = null;
+
+        int maxCount = 0;
         for (int i = 0; i < spString.Length; i++)
         {
-            string[] code = spString[i].Split(',');
-            for (int j = 0; j < code.Length -1; j++)
+            int tempCount = 0;
+            code = spString[i].Split(',');
+            for (int j = 0; j < code.Length - 1; j++)
             {
                 string s = code[j].Replace(" ", "");
                 s = code[j].Replace("\n", "");
-                
+
                 if (s[0].Equals('\n') || s[0].Equals("") || s[0].Equals("\n"))
                     break;
                 if (!s.Equals("0"))
@@ -40,12 +68,19 @@ public class MapLoad : MonoBehaviour
                     obj.transform.localPosition = basicPos + new Vector2(300 * j, 300 * i);
                     obj.transform.localScale = Vector2.one;
                 }
+                tempCount++;
             }
+            if (maxCount < tempCount)
+                maxCount = tempCount;
         }
+        Debug.Log(maxCount);
+        posX += maxCount - 1;
+        posY += spString.Length - 1;
+
         return str;
     }
-
-    // X
+    
+    // 더이상 쓰지 않음 X
     string pathForDocuments(string fileName)
     {
         if (Application.platform == RuntimePlatform.Android)
